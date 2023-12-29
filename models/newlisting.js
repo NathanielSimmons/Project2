b133c895259d18179f70dbc07f8f4f5e8da39fb8
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2023-12-29 12:25:14 -0500

    Updated formmating and added features to Schema

--- models/newlisting.js
+++ models/newlisting.js
@@ -2,10 +2,32 @@ const mongoose = require('mongoose');
 
 const newListingSchema = new mongoose.Schema({
   pictures: [String],
-  bedrooms: Number,
-  bathrooms: Number,
-  sqFootage: Number,
-  reservation: { type: Date, default: null },
+  bedrooms: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  bathrooms:  {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  sqFootage: {
+    type: Number,
+    required: true,
+    min: 500,
+    max: 10000,
+  },
+  reservation: {
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
 
 const Newlisting = mongoose.model('Newlisting', newListingSchema);
